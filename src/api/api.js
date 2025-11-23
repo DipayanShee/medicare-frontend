cc7493a708d0bd4d4119e2e51bd6391207bae6bb
Dipayan Shee <dipayanshee88614@gmail.com>
2025-11-23 21:13:04 +0530

    update backend url for live deployment

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://medicare-backend-2qu5.onrender.com",
+  baseURL: "https://medicare-backend-2qu5.onrender.com/api",
 });
 
 export default API;
